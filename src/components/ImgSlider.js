752a435384a20a9410ce8e11732e966aeb2aa7ba
Kunj Patel <kunjpatel49@gmail.com>
2021-08-02 16:05:12 -0500

    added movie title images

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -3,6 +3,7 @@ import styled from "styled-components"
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+import Overlay from 'react-image-overlay';import { ImageOverlay } from 'react-image-overlay-effect';
 
 function ImgSlider() {
 
@@ -12,16 +13,41 @@ function ImgSlider() {
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
-        autoplay: true
+        autoplay: false
     }
-    
+
     return (
         <Carousel {...settings}>
             <Wrap>
-                <img src="/images/slider-badging.jpg" />
+                <ImageOverlay
+                    src="/images/Black Widow.jfif"
+                    description={<img src={"/images/Black Widow_title.png"} alt={""}/>} />
             </Wrap>
             <Wrap>
-                <img src="/images/slider-badag.jpg" />
+                <ImageOverlay
+                    src="/images/Jungle Cruise.jfif"
+                    description={<img src={"/images/Jungle Cruise_title.png"} alt={""}/>} />
+            </Wrap>
+            <Wrap>
+                <ImageOverlay
+                    src="/images/Monsters at work.jfif"
+                    description={<img src={"/images/Monsters at work_title.png"} style={{width:430, height:242, left: 0}} alt={""}/>} />
+            </Wrap>
+            <Wrap>
+                <ImageOverlay
+                    src="/images/Bad Batch.jfif"
+                    description={<img src={"/images/Bad Batch_title.png"} style={{width:430, height:242}} alt={""}/>} />
+            </Wrap>
+            <Wrap>
+                <ImageOverlay
+                    src="/images/Spiderman and amazing friends.jfif"
+                    description={<img src={"/images/Spiderman and amazing friends_title.png"} alt={""}/>}
+                />
+            </Wrap>
+            <Wrap>
+                <ImageOverlay
+                    src="/images/Stuntman.jfif"
+                    description={<img src={"/images/Stuntman_title.png"} alt={""}/>} />
             </Wrap>
         </Carousel>
     )
@@ -65,6 +91,7 @@ const Wrap = styled.div`
         box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
         rgb(0 0 0 / 73%) 0px 16px 10px -10px;
         transition-duration: 300ms;
+      
 
         &:hover {
             border: 4px solid rgba(249, 249, 249, 0.8);
@@ -72,5 +99,15 @@ const Wrap = styled.div`
         }
 
     }
-
+    .middle {
+        transition: .5s ease;
+        opacity: 0;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        -ms-transform: translate(-50%, -50%);
+        text-align: center;
+    }
+  
 `
