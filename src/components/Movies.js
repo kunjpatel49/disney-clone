4572faf301411c18ad81f30438f579ff1d86d2dc
Kunj Patel <kunjpatel49@gmail.com>
2021-08-02 14:27:43 -0500

    added new images from disney

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -7,28 +7,28 @@ function Movies() {
             <h4>Recommended for You</h4>
             <Content>
                 <Wrap>
-                    <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg" />
+                    <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/3046861447155EC2E2A4D849EFC0C2CC64AD994F0627AAD8819FFE94B8086117/scale?width=400&aspectRatio=1.78&format=jpeg" />
                 </Wrap>
                 <Wrap>
-                    <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg" />
+                    <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/8CB87D9EC43361F6FD2F3513F9DEF0733CB72E383CE0D89E6174C96851F9D575/badging?width=400&aspectRatio=1.78&format=jpeg&label=originals" />
                 </Wrap>
                 <Wrap>
-                    <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg" />
+                    <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/1A6952C3996B77FA5CE0D3653C3A0E1A2BA19D00DEC113BD2C64BC543BDAD6C5/scale?width=400&aspectRatio=1.78&format=jpeg" />
                 </Wrap>
                 <Wrap>
-                    <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg" />
+                    <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/8CBE51B9FD6AC3D0DA841C005AF40AC02CD9AF034F4D05B9E9603B62F2925287/scale?width=800&aspectRatio=1.78&format=jpeg" />
                 </Wrap>
                 <Wrap>
-                    <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg" />
+                    <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/EEA0E8F58B53CE9B921C2E6A55DA8B73B6043C752263C00C662D13441B0DFBEC/scale?width=800&aspectRatio=1.78&format=jpeg" />
                 </Wrap>
                 <Wrap>
-                    <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg" />
+                    <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/A0A3C720DA3016AE87020542157DCB17A70B1410553BCC2AFCAF15C1D52C4002/scale?width=1440&aspectRatio=1.78&format=jpeg" />
                 </Wrap>
                 <Wrap>
-                    <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg" />
+                    <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/A0EB6E13C739DD631FABFE831E32DDB2944B5BBA295041E3A79C3ABDD79BB702/scale?width=400&aspectRatio=1.78&format=jpeg" />
                 </Wrap>
                 <Wrap>
-                    <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg" />
+                    <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/64033DD4698B5885812AB2198C1C97DAEA553C9B66171D29106669DF32735EAF/scale?width=400&aspectRatio=1.78&format=jpeg" />
                 </Wrap>
             </Content>
         </Container>
